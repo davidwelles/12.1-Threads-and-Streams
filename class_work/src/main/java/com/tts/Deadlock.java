b5508a0c285309ca4b7eb1be0aaee138ac7255df
David Welles <74557848+davidwelles@users.noreply.github.com>
2021-05-03 14:48:43 -0400

    second commit with homework added

---
+++ class_work/src/main/java/com/tts/Deadlock.java
@@ -0,0 +1,33 @@
+package com.tts;
+
+public class Deadlock {
+    static class Friend{
+        private final String name;
+
+        public Friend(String name){
+            this.name = name;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public synchronized void bow(Friend bower){
+            System.out.format("%s: %s has bowed to me! %n", this.getName(), bower.getName());
+            bower.bowBack(this);
+        }
+
+        public synchronized void bowBack(Friend bower){
+            System.out.format("%s: %s has bowed to me! %n", this.getName(), bower.getName());
+        }
+    }
+
+    public static void main(String[] args){
+        final Friend alphonse = new Friend("Alphonse");
+        final Friend gaston = new Friend("gaston");
+
+        new Thread(() -> alphonse.bow(alphonse)).start();
+        new Thread(() -> gaston.bow(gaston)).start();
+    }
+}
+//the above code is example of how synchronization can create deadlock in your program
